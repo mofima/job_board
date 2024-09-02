1cf563310fa57ecbfafbbb737f912e32581b8907
jimogeto <jimogeto@gmail.com>
2024-09-02 11:59:40 +0300

    added all env vars to env_local and added env_local to web and db service in docker-compose

--- job_board/settings/local.py
+++ job_board/settings/local.py
@@ -13,3 +13,16 @@ environ.Env.read_env(os.path.join(BASE_DIR, 'settings', '.env_local'))
 DEBUG = env.bool('DEBUG', default=True) # set true for local devpt
 SECRET_KEY = env('SECRET_KEY', default='your-default-secret-key')
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['localhost', '127.0.0.1'])
+
+
+# Database settings for PostgreSQL
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('POSTGRES_DB'),
+        'USER': env('POSTGRES_USER'),
+        'PASSWORD': env('POSTGRES_PASSWORD'),
+        'HOST': env('POSTGRES_HOST'),  # Use 'db' when running in Docker
+        'PORT': env('POSTGRES_PORT'),
+    }
+}
